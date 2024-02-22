875cc3eb2d40e9462a99a25910f21835b4432c23
cao minh quang <cquang2k5@gmail.com>
2024-02-22 09:51:56 +0700

    đã thêm foder HW1

---
+++ HW1/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Cao Minh Quang    23020411    AI1    quang011105    cquang2k5@gmail.com");
+        for (int i = 9; i >=1; i--) {
+            System.out.println(i+" bottles of beer on the wall, "+i+" bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+
+}
